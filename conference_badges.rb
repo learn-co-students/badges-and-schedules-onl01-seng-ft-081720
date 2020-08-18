4689c668672dc74d98fc3f560dff3709abd4f792
Alma Flores <flores.almalee@gmail.com>
2020-08-18 03:19:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name) 
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  array.each{|name|"Hello, my name is #{name}" }
+end
+
+def assign_rooms(array)
+  array.each do |name|
+    room = 1
+    while room <= 7
+      "Hello, #{name}! You'll be assigned to room #{room}!"
+      room += 1
+    end
+  end
+end
