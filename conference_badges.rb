3167ced7a220a75041f75473d1d431ecbc9f644f
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-19 01:21:07 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    result=[]
+    arr.each do |i|
+        result.push(badge_maker(i))
+    end
+    result 
+end
+
+def assign_rooms(list)
+    room=[]
+    list.each_with_index do |item, index|
+        room.push("Hello, #{item}! You'll be assigned to room #{index+1}!")
+    end
+    room
+end
+
+def printer (list)
+    
+    batch_badge_creator(list).each do |i|
+     puts i
+    end 
+    assign_rooms(list).each do |j|
+     puts j
+    end 
+ end
