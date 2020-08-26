5ae44465eb73155a06086344a06d41b62217b32f
selam lemma <oneiconforlife26@gmail.com>
2020-08-26 22:05:22 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+   "Hello, my name is #{name}."
+end
+end
+   ##batch_badge_creator
+def assign_rooms(speakers)
+  index = 0
+  speakers.map do |speaker|
+"Hello, #{speaker}! You'll be assigned to room #{index +=1}!"
+end
+end
+
+def printer(name)
+  name.map do |name|
+  "Hello, my name is #{name}."
+end
+end
+
+def printer(printer)
+printer.map do|printer|
+  "Hello, my name is #{printer}."
+end
+end
